5890e541ecb6b6fca2c95f6aadf90834442769f4
Fedor Shatalov <Boss.gooog@gmail.com>
2024-06-17 09:50:21 +0300

    add first Multilevel Menu

---
+++ Not Sites -__-/Multilevel-Menu/Menu-one/README.md
@@ -0,0 +1,9 @@
+# Practice
+
+_My first Multilevel-Menu_ 😊🔺🔻
+## Stack: 
+- HTML 5 
+- CSS 3 
+
+## Website Launch
+You can clone the repository and open index.html
---
+++ Not Sites -__-/Multilevel-Menu/Menu-one/js/main.js
@@ -0,0 +1,11 @@
+// const mainItem = document.querySelector('.main-item');
+// const subItem = document.querySelector('.sub-menu');
+// const click = 0;
+// mainItem.onclick = function(){
+//   click+1;
+// }
+// if (click % 2 === 0){
+//   mainItem.addEventListener('click', ()=>{
+//     subItem.classList.toggle('open');
+//   });
+// }
