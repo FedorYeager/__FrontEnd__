e9c79c0755e36cd6c75f4596f9689312ee9408a0
Fedor Shatalov <Boss.gooog@gmail.com>
2024-06-18 10:30:42 +0300

    add comments GPT in main.js multilevel menu 2

--- Not Sites -__-/Multilevel-Menu/Menu-two/js/main.js
+++ Not Sites -__-/Multilevel-Menu/Menu-two/js/main.js
@@ -6,18 +6,18 @@ let isMobile = {
   Windows: function() {return navigator.userAgent.match(/IEMobile/i);},
   any: function() {return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());}
 };
-let body =document.querySelector('body');
-if(isMobile.any()){
+let body=document.querySelector('body');
+if (isMobile.any()) {
   body.classList.add('touch');
-  let arrow=document.querySelectorAll('.arrow');
-  for(i=0; i<arrow.length; i++){
-    let thisLink=arrow[i].previousElementSibling;
-    let subMenu=arrow[i].nextElementSibling;
-    let thisArrow=arrow[i];
-    thisLink.classList.add('parent');
-    arrow[i].addEventListener('click', function(){
-      subMenu.classList.toggle('open');
-      thisArrow.classList.toggle('active');
+  let arrow=document.querySelectorAll('.arrow'); // Получаем все элементы с классом 'arrow'
+  for (i=0; i<arrow.length; i++) { // Цикл for, который перебирает все элементы с классом 'arrow'
+    let thisLink=arrow[i].previousElementSibling; // Получаем предыдущий элемент (предположительно ссылку)
+    let subMenu=arrow[i].nextElementSibling; // Получаем следующий элемент (предположительно подменю)
+    let thisArrow=arrow[i]; // Сохраняем текущую стрелку в переменную
+    thisLink.classList.add('parent'); // Добавляем класс 'parent' к предыдущему элементу (ссылке)
+    arrow[i].addEventListener('click', function() { // Добавляем обработчик события на клик для текущей стрелки
+      subMenu.classList.toggle('open'); // Переключаем класс 'open' у подменю
+      thisArrow.classList.toggle('active'); // Переключаем класс 'active' у стрелки
     });
   }
 }else{
