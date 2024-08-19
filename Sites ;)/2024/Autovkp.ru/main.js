1ad43a5a216fd0f895fa2c076e05ab648475c184
Fedor Shatalov <Boss.gooog@gmail.com>
2024-08-19 17:10:35 +0300

    add sections models and location

---
+++ Sites ;)/2024/Autovkp.ru/main.js
@@ -0,0 +1,128 @@
+const burgerButton = document.querySelector('.burger-btn')
+const menu = document.querySelector('.header__list')
+burgerButton.addEventListener('click', ()=> {
+  menu.classList.toggle('open-menu')
+})
+
+
+// ! ========================================
+
+
+const showMoreButtonModels = document.querySelector('.models__button')
+const modelsColumns = Array.from(document.querySelectorAll('.models__column'))
+
+function openColumnsModel(){
+  showMoreButtonModels.addEventListener('click', () => {
+    modelsColumns.forEach(item => item.classList.remove('visually-hidden'));
+    showMoreButtonModels.classList.add('visually-hidden')
+  })
+}
+
+function responseModelOne() {
+  if (window.innerWidth > 1300){
+    showMoreButtonModels.classList.add('visually-hidden')
+    modelsColumns.forEach((item, index) => {
+      item.classList.add('visually-hidden')
+      if (index <= 6){
+        item.classList.remove('visually-hidden')
+      } else if (index > 6){
+        showMoreButtonModels.classList.remove('visually-hidden')
+      }
+    })
+    openColumnsModel()
+  }
+}
+responseModelOne()
+
+function responseModelTwo() {
+  if (window.innerWidth > 1024 && window.innerWidth < 1300){
+    showMoreButtonModels.classList.add('visually-hidden')
+    modelsColumns.forEach((item, index) => {
+      item.classList.add('visually-hidden')
+      if (index <= 4){
+        item.classList.remove('visually-hidden')
+      } else if (index > 4){
+        showMoreButtonModels.classList.remove('visually-hidden')
+      }
+    })
+    openColumnsModel()
+  }
+}
+responseModelTwo()
+
+function responseModelThree() {
+  if (window.innerWidth > 767 && window.innerWidth < 1024){
+    showMoreButtonModels.classList.add('visually-hidden')
+    modelsColumns.forEach((item, index) => {
+      item.classList.add('visually-hidden')
+      if (index <= 3){
+        item.classList.remove('visually-hidden')
+      } else if (index > 3){
+        showMoreButtonModels.classList.remove('visually-hidden')
+      }
+    })
+    openColumnsModel()
+  }
+}
+responseModelThree()
+
+function responseModelFour() {
+  if (window.innerWidth < 767){
+    showMoreButtonModels.classList.add('visually-hidden')
+    modelsColumns.forEach((item, index) => {
+      item.classList.add('visually-hidden')
+      if (index <= 2){
+        item.classList.remove('visually-hidden')
+      } else if (index > 2){
+        showMoreButtonModels.classList.remove('visually-hidden')
+      }
+    })
+    openColumnsModel()
+  }
+}
+responseModelFour()
+
+
+// ! ========================================
+
+const showMoreButtonLocation = document.querySelector('.location__button')
+const locationsColumns = Array.from(document.querySelectorAll('.location__column'))
+
+function openColumnsLocation(){
+  showMoreButtonLocation.addEventListener('click', () => {
+    locationsColumns.forEach(item => item.classList.remove('visually-hidden'));
+    showMoreButtonLocation.classList.add('visually-hidden')
+  })
+}
+
+function responseLocationOne() {
+  if (window.innerWidth > 767){
+    showMoreButtonLocation.classList.add('visually-hidden')
+    locationsColumns.forEach((item, index) => {
+      item.classList.add('visually-hidden')
+      if (index <= 3){
+        item.classList.remove('visually-hidden')
+      } else if (index > 3){
+        showMoreButtonLocation.classList.remove('visually-hidden')
+      }
+    })
+    openColumnsLocation()
+  }
+}
+responseLocationOne()
+
+function responseLocationTwo() {
+  if (window.innerWidth < 767){
+    showMoreButtonLocation.classList.add('visually-hidden')
+    locationsColumns.forEach((item, index) => {
+      item.classList.add('visually-hidden')
+      if (index <= 1){
+        item.classList.remove('visually-hidden')
+      } else if (index > 1){
+        showMoreButtonLocation.classList.remove('visually-hidden')
+      }
+    })
+    openColumnsLocation()
+  }
+}
+responseLocationTwo()
